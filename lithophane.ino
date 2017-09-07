b0f74ac5e7edc7671eaaca891920b8e3b1739948
Oscar R.C <oscarrc@users.noreply.github.com>
2017-09-07 23:18:09 +0200

    Rewrite to reduce flickering
    Reducing flickering by not writing while pot is not moving.

--- lithophane.ino
+++ lithophane.ino
@@ -3,12 +3,13 @@
 #define NEO_PIN 0   //Neopixels conected to pin
 #define NEO_NUM 16  //Number of neopixels
 #define POT_PIN 1   //Pot connected to pin
-#define BRIGHTNESS 255 //Neopixel full brightness
 
-//RGBW neopixel stick
+//RGBW Neopixel stick
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEO_NUM, NEO_PIN, NEO_RGBW + NEO_KHZ800);
 
-int potVal = 550;
+int potVal = 0;
+int potNew = 0;
+int potDelta = 0;
 int red, green, blue, white;
 
 void setup()
@@ -27,44 +28,51 @@ void setup()
 // Main program
 void loop()
 {
-    potVal = analogRead(POT_PIN);
+    potNew = analogRead(POT_PIN);
+    potDelta = abs(potVal - potNew);
 
-    if (potVal < 256){	// Lowest quarter of the potentiometer's range (0-255)
-                    
-        potVal = map(potVal, 0, 255, 0, 255);	// Normalize to 0-255
+    if (potDelta > 2){
+      potVal = potNew;  
 
-        white = 256 - potVal;	// From full to off
-        red = potVal;			// Red from off to full
-        green = 1;				// Green from off to full
-        blue = 1;				// Blue off
+      switch(potVal){
+        case 0 ... 255: // Lowest quarter of the potentiometer's range (0-255)
         
-    }else if (potVal < 512){	// Second quarter of potentiometer's range (256-511)
- 
-        potVal = map(potVal, 256, 511, 0, 255);	// Normalize to 0-255
-
-        white = 1;			// White off
-        red = 256 - potVal;	// Red from full to off
-        green = potVal;		// Green from off to full
-        blue = 1;			// Blue off
+          potVal = map(potVal, 0, 255, 0, 255);  // Normalize to 0-255
+          white = 256 - potVal; // From full to off
+          red = potVal;     // Red from off to full
+          green = 1;        // Green from off to full
+          blue = 1;       // Blue off
+          
+          break;
+        case 256 ... 511: // Second quarter of potentiometer's range (256-511)
         
-    }else if (potVal < 768){	// Third quarter of potentiometer"s range (512-767)
-  
-        potVal = map(potVal, 512, 767, 0, 255); // Normalize to 0-255
-
-        white = 1;				// White from off to full
-        red = 1;				// Red from off to full
-        green = 256 - potVal;	// Green from full to off
-        blue = potVal;			// Blue from off to full
+          potVal = map(potVal, 256, 511, 0, 255);  // Normalize to 0-255
+          white = 1;      // White off
+          red = 256 - potVal; // Red from full to off
+          green = potVal;   // Green from off to full
+          blue = 1;     // Blue off
+          
+          break; 
+        case 512 ... 767: // Third quarter of potentiometer"s range (512-767)
         
-    }else{
-        potVal = map(potVal, 767, 1023, 0, 255);	// Normalize to 0-255
-
-        white = potVal;			// White from off to full
-        red = 1;				// Red from off to full
-        green = 1;				// Green off
-        blue = 256 - potVal;	// Blue full to off
+          potVal = map(potVal, 512, 767, 0, 255); // Normalize to 0-255
+          white = 1;        // White from off to full
+          red = 1;        // Red from off to full
+          green = 256 - potVal; // Green from full to off
+          blue = potVal;      // Blue from off to full
+          
+          break;
+        default:
+        
+          potVal = map(potVal, 767, 1023, 0, 255);  // Highest quarter of potentiometer"s range (512-767)
+          white = potVal;     // White from off to full
+          red = 1;        // Red from off to full
+          green = 1;        // Green off
+          blue = 256 - potVal;  // Blue full to off
+          
+          break;
       }
-    
+
       //Write values to strip
       for( int i = 0; i < NEO_NUM; i++){
           strip.setPixelColor(i, red, green, blue, white);
@@ -72,4 +80,5 @@ void loop()
 
       //Show color in neopixel
       strip.show();
+    }
 }
